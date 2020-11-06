bcc972c05a64cda201a05006839c7bb71c4b2556
Kannan Ramamoorthy <kannangce@rediffmail.com>
2020-11-06 11:58:52 +0530

    Update frequency.clj

--- src/frequency.clj
+++ src/frequency.clj
@@ -2,7 +2,8 @@
 
 (require
    '[clojure.tools.cli :refer [parse-opts]]
-   '[clojure.java.io :as io])
+   '[clojure.java.io :as io]
+   '[clojure.string :as string])
 
 (def progname "fequencies")
 
@@ -12,20 +13,53 @@
    ["-d" "--descending" "Sort in descending order"]
    ["-h" "--help"]])
 
+(defn print-usage
+  [progname summary]
+  (println "usage: " progname " [opts]")
+  (println " ")
+  (println "options:")
+  (println summary))
+
+(defn find-errors
+  [parsed]
+  (let [{:keys [errors options]} parsed
+        {:keys [help]} options]
+    (cond
+      help
+      {:exit 0}
+
+      errors
+      {:message (string/join "\n" errors)
+       :exit 1}
+      )))
+
+(defn print-errors
+  [progname parsed errors]
+  (let [{:keys [summary]} parsed
+        {:keys [message exit]} errors]
+    (when message
+      (println message)
+      (println " "))
+    (print-usage progname summary)
+    exit))
+
+
 (defn format
   [data options]
-  (reduce #(str %1 "\t"  %2 "\n") result))
+  (do (println data)
+      (println options)
+    (reduce #(str %1 (first %2) "\t"  (second %2) "\n") "" data)))
 
 (defn get-data
 [options]
-(or (:file options) (*input*)))
+(or (:file options) *input*))
 
 
-(defn -main [& args]
-  (let [parsed (parse-opts args cli-options)
-        {:keys [options]} parsed]
-    (or (some->> (opts/find-errors parsed)
-                 (opts/print-errors progname parsed)
-                 (System/exit))
-        (-> (get-data options)
-        	(format options)))))
+(let [parsed (parse-opts *command-line-args* cli-options)
+      {:keys [options]} parsed]
+  (or (some->> (find-errors parsed)
+               (print-errors progname parsed)
+               (System/exit))
+      (-> (get-data options)
+        (frequencies)
+        (format options))))

a5a5a8174c9ebe922376628d2acd0f1cd36643df
Kannan Ramamoorthy <kannangce@rediffmail.com>
2020-11-16 14:51:12 +0530

    Working version

--- src/frequency.clj
+++ src/frequency.clj
@@ -3,15 +3,17 @@
 (require
    '[clojure.tools.cli :refer [parse-opts]]
    '[clojure.java.io :as io]
-   '[clojure.string :as string])
+   '[clojure.string :as str])
 
 (def progname "fequencies")
+(def ^:dynamic *debug* false)
 
 (def cli-options
   [["-f" "--file FILE" "File to process" :parse-fn #(slurp %)]
    ["-s" "--sort SORT" "Sort column" :validate [#(<= 1 % 2) :default 1]]
    ["-d" "--descending" "Sort in descending order"]
-   ["-h" "--help"]])
+   ["-h" "--help"]
+   ["-D" "--debug"]])
 
 (defn print-usage
   [progname summary]
@@ -27,9 +29,8 @@
     (cond
       help
       {:exit 0}
-
       errors
-      {:message (string/join "\n" errors)
+      {:message (str/join "\n" errors)
        :exit 1}
       )))
 
@@ -43,23 +44,35 @@
     (print-usage progname summary)
     exit))
 
-
 (defn format
   [data options]
-  (do (println data)
-      (println options)
-    (reduce #(str %1 (first %2) "\t"  (second %2) "\n") "" data)))
+    (reduce-kv #(str %1  %2 "\t"  %3 "\n") "" data))
 
 (defn get-data
 [options]
-(or (:file options) *input*))
+(if (:file options)
+  (str/split (:file options) #"\n") 
+  *input*))
 
+(defn debug 
+  ([dat msg]
+	(when (true? *debug*)
+   (println "**Debug**" msg)
+   (println dat)
+   (println "*******"))
+  dat)
+  ([dat]
+   (debug dat "")))
 
 (let [parsed (parse-opts *command-line-args* cli-options)
       {:keys [options]} parsed]
   (or (some->> (find-errors parsed)
                (print-errors progname parsed)
                (System/exit))
-      (-> (get-data options)
-        (frequencies)
-        (format options))))
+      (binding [*debug* (:debug options)]
+        (-> (get-data options)
+            (debug "input data")
+            (frequencies)
+            (debug "frequency")
+            (format options)
+            (print)))))

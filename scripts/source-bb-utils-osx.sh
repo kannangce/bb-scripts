a8ca20c75d66bef37b5dce0cd6301154361bbbbb
Kannan Ramamoorthy <kannangce@rediffmail.com>
2021-01-28 12:12:25 +0530

    Added some alias and functions

--- scripts/source-bb-utils-osx.sh
+++ scripts/source-bb-utils-osx.sh
@@ -3,3 +3,16 @@ function frequency()
 {
  bb -i -f $BB_SCRIPTS/src/frequency.clj -- $@
 }
+
+# mkdir and cd
+function mkcd() { mkdir $1; cd $1 ;}
+
+# git push for new branch
+function push() { echo "Pushing to $(git branch --show-current)"; git push --set-upstream origin $(git branch --show-current);}
+
+## Composed with clipboard
+# format json
+alias jlint='pbpaste|jq . |pbcopy'
+
+# deduplicate
+alias dedup='pbpaste| sort -u|pbcopy'
